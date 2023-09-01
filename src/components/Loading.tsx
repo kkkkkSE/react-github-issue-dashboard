825c3d857bde08a1b241eb81792d1624b694ad7d
kkkkkse <eun2abc@gmail.com>
2023-09-01 15:22:58 +0900

    [design] 로딩 컴포넌트 스타일링

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,13 +1,34 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export default function Loading() {
   return (
     <Container>
-      Loading...
+      <div />
     </Container>
   );
 }
 
-const Container = styled.p`
-  ${(props) => props.theme.texts.bold.subHeader}
+const rotation = keyframes`
+  from{
+      transform: rotate(0deg);
+  }
+
+  to{
+      transform: rotate(360deg);
+  }
+`;
+
+const Container = styled.div`
+  padding-block: 4rem;
+
+  div {
+    height: 30px;
+    width: 30px;
+    border: .2rem solid ${(props) => props.theme.colors.gray700};
+    border-radius: 50%;
+    border-top: none;
+    border-right: none;
+    margin: 16px auto;
+    animation: ${rotation} 1s linear infinite;  
+  }
 `;
